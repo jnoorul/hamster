d07e12a50a563c057c9b7ff206130b4228574576
jnoorul <noorul.it@gmail.com>
2017-10-26 08:06:22 +0800

    build folder

---
+++ build/server.js
@@ -0,0 +1,11 @@
+/* eslint-disable */
+var express = require('express');
+const app = express();
+const port = process.env.PORT || 3000;
+
+app.use(express.static('build'));
+
+app.listen(port, ()=>{
+  console.log(`server started using port:${port}`);
+});
+
