89fd4160ada17ec3d470fb17bdcfa9cd1090be46
jnoorul <noorul.it@gmail.com>
2017-10-28 22:13:08 +0800

    test

--- public/server.js
+++ public/server.js
@@ -1,7 +1,7 @@
 /* eslint-disable */
 var express = require('express');
 const app = express();
-const port = process.env.port || 4000;
+const port = process.env.PORT || '3000';
 
 app.use(express.static('build'));
 
