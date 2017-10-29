f22080d4dff5d802379c6f79a2b552d6a04d2be9
Raghuveer Singh <raghu1213@hotmail.com>
2017-10-29 10:49:48 +0800

    line style

--- src/index.js
+++ src/index.js
@@ -1,8 +1,9 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import 'antd/dist/antd.css';
 import Hamster from './container/Hamster';
 import registerServiceWorker from './registerServiceWorker';
-import 'antd/dist/antd.css';
 import './assets/hamster.css';
+
 ReactDOM.render(<Hamster />, document.getElementById('root'));
 registerServiceWorker();
