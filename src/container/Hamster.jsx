bd8133f2e2dd7ae385947134c98f43b6c003614a
user <user@MacBook-Pro.local>
2017-11-04 19:54:05 +0530

    test

--- src/container/Hamster.jsx
+++ src/container/Hamster.jsx
@@ -1,8 +1,7 @@
 import React from 'react';
 import PropTypes from 'prop-types';
 import { Grid } from 'semantic-ui-react';
-import CustomerInfo from '../components/CustomerInfo';
-import Portfolio from '../components/Portfolio';
+import WelcomePage from '../components/WelcomePage';
 
 const Hamster = class Hamster extends React.Component {
   constructor(props) {
@@ -17,18 +16,8 @@ const Hamster = class Hamster extends React.Component {
     return (
       <div>
         <div className="header" style={{ color: 'white' }}>Hamster</div>
-        <div>{this.props.customerName}</div>
-        <br />
-        <Grid>
-          <Grid.Row>
-            <Grid.Column width={5}>
-              <CustomerInfo />
-            </Grid.Column>
-            <Grid.Column width={11}>
-              <Portfolio />
-            </Grid.Column>
-          </Grid.Row>
-        </Grid>
+        <WelcomePage />
+        <div className="footer" style={{ color: 'white' }}>Hamster</div>
       </div>);
   }
 
