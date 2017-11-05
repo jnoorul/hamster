93a402fc89e87b05b881ab6dfd1e24d95b46335b
anilmodest <modest.anil@gmail.com>
2017-11-06 03:30:53 +0800

    login

---
+++ src/assets/PortfolioBreakdown.js
@@ -0,0 +1,11 @@
+const PortfolioDivision = [];
+// rectangle and rectangle test cases
+PortfolioDivision.push({
+  'Mutual Funds': 8.3,
+  Stocks: 10.1,
+  'Fixed Income': 39.6,
+  Cash: 20,
+  ETF: 22,
+});
+
+module.exports = PortfolioDivision
